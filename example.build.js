3173ec0450c7cce2103297757a92932ffbb6200f
Dai <dai.williams@bbc.co.uk>
2015-08-07 00:56:00 +0100

    added subheader to card

--- example.build.js
+++ example.build.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const data = Object.freeze({
   
   location: 'Porthcawl Rest Bay',
+  subheader: 'August 15th 2015',
   swell: ['1-2', '1-4', '2-3', '4-5', '1-2', '1-2'],
   time: ['10am', '12pm', '2pm', '4pm', '6pm', '1-2'],
   windspeed: ['11', '10', '13', '33', '19', '1-2'],
